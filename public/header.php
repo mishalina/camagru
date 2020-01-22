02e127c62bfc511fb72ac0e7a1f7feb14d3ed401
Mishalina <mishchenko.alina@gmail.com>
2020-01-22 21:54:43 +0300

    add some icons

--- public/header.php
+++ public/header.php
@@ -3,7 +3,7 @@
 	<?php
 	$root = "http://".$_SERVER['HTTP_HOST'].substr($_SERVER['SCRIPT_NAME'], 0, (strpos($_SERVER['SCRIPT_NAME'], "/", 1) + 1));
 	echo "
-		<li><a href='".$root."../index.php'>Camera</a></li>
+		<li><a href='".$root."../index.php'>Home</a></li>
 		<li><a href='".$root."public/gallery.php'>Gallery</a></li>
 		<li><a href='".$root."public/about.php'>About</a></li>";
 		if (isset($_SESSION['logged']))
